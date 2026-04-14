eb47aa1630afb3ea9e459581b78de41df8834071
WildHuntSA <wildhuntsa@gmail.com>
2026-04-14 18:40:50 +0200

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,7 +1,7 @@
 // WildHunt SA Service Worker
 // Enables offline support and installability
 
-const CACHE_NAME = 'wildhuntsa-v8';
+const CACHE_NAME = 'wildhuntsa-v10';
 const OFFLINE_URL = '/';
 
 // Assets to cache on install
